a77e8c2422ee7fd935f689ba6c89fd522ddd760f
Richard Killam <richard.killam@gmail.com>
2015-08-31 17:20:28 -0300

    Added (simple) programming problems and description of comments in Python to cheat sheet

---
+++ LearnCython/Week1/ProgrammingProblems/p1_number_sum_even_vs_odd.py
@@ -0,0 +1,17 @@
+#####################################################################
+# Problem 1: Number Sum Even vs Odd
+#
+# Ask the user for a number, give them a quit option (say -1 to quit)
+# Calculate the sum of all number from 1 to their input (1+2+3+...+n)
+# Output the value and say if it is even or odd
+#
+# Example inputs and output:
+#   n =  1 ->    1 is odd
+#   n =  2 ->    3 is odd
+#   n =  3 ->    6 is odd
+#   n =  4 ->   10 is odd
+#   n =  5 ->   15 is odd
+#   n =  8 ->   36 is even
+#   n = 25 ->  325 is odd
+#   n = 75 -> 2850 is even
+#####################################################################
---
+++ LearnCython/Week1/ProgrammingProblems/p2_my_list.py
@@ -0,0 +1,21 @@
+#####################################################################
+# Problem 2: My List Class
+#
+# Write a class which will hold a list of items. The classes'
+# constructor should take the maximum size of the list (note that
+# Python lists are expandable, that is, they don't have a max length)
+#
+# This class will then have 2 methods
+#   1: add, adds a given element to the list, but removes the first
+#      element if there are too many in the list (queue)
+#   2: has, looks through the list to see if a given item is there,
+#      returns True if it is, False otherwise
+#
+# Then write a simple interface which allows the user to add elements
+# to their list
+#
+# You haven't seen any examples of removing an item from a Python
+# list. You should try to figure it out yourself (Google is your
+# friend for all programming questions) but since that isn't and
+# important part of the workshop you can ask me and I'll tell you.
+#####################################################################
